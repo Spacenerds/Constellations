1d9e3a75e7b2dfe725fb5c8f0cf0206839c8ea43
Elizabeth <ehuntsman@wavetronix.com>
2017-08-10 15:38:11 -0600

    added Tarot View

---
+++ src/components/Tarot/TarotView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TarotView extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Tarot Reading</h1>
+            </div>
+        );
+    }
+}
+
+export default TarotView;
--- src/router.js
+++ src/router.js
@@ -5,11 +5,13 @@ import {Switch, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
 import Events from './components/Events/Events';
 import Constellations from './components/ConstellationMapping';
+import TarotView from './components/Tarot/TarotView';
 
 export default(
     <Switch>
         <Route exact component={Home} path='/' />
         <Route component={Events} path='/events' />
         <Route component={Constellations} path='/constellations' />
+        <Route component={TarotView} path='/tarot' />
     </Switch>
 )
