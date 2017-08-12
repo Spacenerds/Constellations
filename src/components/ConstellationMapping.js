e74c052d86db579f73eed49bd8802cc62ce565d9
Eli Keetch <elikeetch1996@gmail.com>
2017-08-12 12:02:03 -0600

    fixed conflicts on local drive

--- src/components/ConstellationMapping.js
+++ src/components/ConstellationMapping.js
@@ -1,59 +1,8 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
+
 import './../App.css';
 
 
-$(document).ready(function() {
-$("div.Layer_10").on("click", function(){
-  $("span.toggled_10").toggleClass("toggle_off");
-  
-});
-
-$("div.Layer_1").on("click", function(){
-  $("div.display_Box").toggleClass("toggle_off");
-  
-});
-$("div.Layer_2").on("click", function(){
-  $("span.toggled_2").toggleClass("toggle_off");
-  
-});
-$("div.Layer_3").on("click", function(){
-  $("span.toggled_3").toggleClass("toggle_off");
-  
-});
-$("div.Layer_4").on("click", function(){
-  $("span.toggled_4").toggleClass("toggle_off");
-  
-});
-$("div.Layer_5").on("click", function(){
-  $("span.toggled_5").toggleClass("toggle_off");
-  
-});
-$("div.Layer_6").on("click", function(){
-  $("span.toggled_6").toggleClass("toggle_off");
-  
-});
-$("div.Layer_7").on("click", function(){
-  $("span.toggled_7").toggleClass("toggle_off");
-  
-});
-$("div.Layer_8").on("click", function(){
-  $("span.toggled_8").toggleClass("toggle_off");
-  
-});
-$("div.Layer_9").on("click", function(){
-  $("span.toggled_9").toggleClass("toggle_off");
-  
-});
-$("div.Layer_11").on("click", function(){
-  $("span.toggled_11").toggleClass("toggle_off");
-  
-});
-$("div.Layer_12").on("click", function(){
-  $("span.toggled_12").toggleClass("toggle_off");
-  
-});
-})
 class ConstellationMapping extends Component {
 
   
@@ -79,20 +28,10 @@ class ConstellationMapping extends Component {
         <div  id='Aquarius' className='Aquarius'></div>
         <div id='Pisces' className='Pisces'></div>
         </div>
-      <div id="t1" className='display_Box'>
-      <span id="spanid" className='toggled_1'>  HELLO I AM WORKINGGG </span>
+      <div className='display_Box'>
+      
       </div>
-      <span id="spanid" className='toggled_2'>  HELLO I AM WORKINGGG </span>
-      <span id="spanid" className='toggled_3'>  HELLO I AM WORKINGGG </span>
-      <span id="spanid" className='toggled_4'>  HELLO I AM WORKINGGG </span>
-      <span id="spanid" className='toggled_5'>  HELLO I AM WORKINGGG </span>
-      <span id="spanid" className='toggled_6'>  HELLO I AM WORKINGGG </span>
-      <span id="spanid" className='toggled_7'>  HELLO I AM WORKINGGG </span>
-      <span id="spanid" className='toggled_8'>  HELLO I AM WORKINGGG </span>
-      <span id="spanid" className='toggled_9'>  HELLO I AM WORKINGGG </span>
-      <span id="spanid" className='toggled_10'>  HELLO I AM WORKINGGG </span>
-      <span id="spanid" className='toggled_11'>  HELLO I AM WORKINGGG </span>
-      <span id="spanid" className='toggled_12'>  HELLO I AM WORKINGGG </span> 
+       
       </div>
       </div>
       </div>
