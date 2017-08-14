534063867a145a7374cd4a3a0778be9980f351ac
Jhonatan Castillo <yekoxcore@gmail.com>
2017-08-14 15:57:46 -0600

    new stuff added -jc

--- src/components/Horoscope/Horoscope.js
+++ src/components/Horoscope/Horoscope.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 import Icons from './Icons';
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+import './Horoscope.css'
+import Background from './hororbg.jpg';
+=======
 import './Search.css'
 import Nav from '.././Nav';
+>>>>>>> Stashed changes
+=======
+import './Search.css'
+import Nav from '.././Nav';
+>>>>>>> Stashed changes
 
 class Horoscope extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          json: {}
+        }
+    }
+
+    componentDidMount () {
+        const URL = 'https://aztro.herokuapp.com/?sign=aries&day=today';
+        fetch(URL, {
+            method: 'POST'
+        }).then(response => response.json())
+        .then(json => { this.setState({json}); });
+    }
+
     render(){
         return(
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+            <div style={styles.itemsStyles}>
+                <Icons />
+=======
+=======
+>>>>>>> Stashed changes
             <div>
                 {/*<Icons />
                 <FormGroup id="search">
@@ -22,11 +54,30 @@ class Horoscope extends Component {
                 </InputGroup>
                  
         </FormGroup>*/}
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
             </div>
         );
     }
 };
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+const styles = {
+    itemsStyles: {
+        height: '100vh',
+        flexDirection: 'column',
+        backgroundImage: `url(${Background})`,
+        backgroundSize: 'cover'
+    }
+}
 
+=======
+>>>>>>> Stashed changes
+=======
+
+>>>>>>> Stashed changes
 
 export default Horoscope;
