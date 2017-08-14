cfbcfe1ed3e2e9e2485030c7cca036c9cb4639a0
Elizabeth <ehuntsman@wavetronix.com>
2017-08-14 14:09:32 -0600

    tarot update add description

--- src/components/Tarot/TarotView.js
+++ src/components/Tarot/TarotView.js
@@ -6,9 +6,18 @@ import Nav from '.././Nav';
 
 
 class TarotView extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            tarot: [],
+        }
+    }
     componentDidMount(){
         axios.get('/tarot').then( (response) => {
-            console.log("cakeeeeeee", response);
+            response.data;
+            this.setState({
+                tarot: response.data
+            })
         })
     }
     render() {
@@ -16,7 +25,7 @@ class TarotView extends Component {
             <div className="tarot-container">
                 <h1>Tarot Reading</h1>
                 <div className="pickAcard-container">
-                    <PickACard/>
+                    <PickACard tarot={this.state.tarot}/>
                 </div>
                 <p>use database call to find card description</p>
                 <p>state should probably be in the parent component?</p>
