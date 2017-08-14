40e188acc6686e37c719974935ea78b1611fe4ee
Jhonatan Castillo <yekoxcore@gmail.com>
2017-08-14 16:13:47 -0600

    replaced old menu with new menu

--- src/components/Horoscope/Horoscope.js
+++ src/components/Horoscope/Horoscope.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import Icons from './Icons';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import './Horoscope.css'
 import Background from './hororbg.jpg';
-=======
 import './Search.css'
 import Nav from '.././Nav';
->>>>>>> Stashed changes
-=======
-import './Search.css'
-import Nav from '.././Nav';
->>>>>>> Stashed changes
+
+
 
 class Horoscope extends Component {
     constructor(props){
@@ -31,40 +25,13 @@ class Horoscope extends Component {
 
     render(){
         return(
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
             <div style={styles.itemsStyles}>
                 <Icons />
-=======
-=======
->>>>>>> Stashed changes
-            <div>
-                {/*<Icons />
-                <FormGroup id="search">
-                <InputGroup>
-                    <FormControl className="inputf" type="text" placeholder="Entere your birthdate" 
-                    onKeyPress={event => {
-                        if (event.key === 'Enter') {
-                            this.props.search(this.state.query)
-                        }
-                    }}/>
-                    <InputGroup.Addon onClick={() => this.props.search(this.state.query)} >
-                        <Glyphicon glyph="search"></Glyphicon>
-                    </InputGroup.Addon>
-                </InputGroup>
-                 
-        </FormGroup>*/}
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
             </div>
         );
     }
 };
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 const styles = {
     itemsStyles: {
         height: '100vh',
@@ -73,11 +40,5 @@ const styles = {
         backgroundSize: 'cover'
     }
 }
-
-=======
->>>>>>> Stashed changes
-=======
-
->>>>>>> Stashed changes
 
 export default Horoscope;
