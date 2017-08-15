44acb166fae87a367a361d6d7fe17e136be5ba48
Elizabeth <ehuntsman@wavetronix.com>
2017-08-15 15:31:35 -0600

    create constellation

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { findDOMNode } from 'react-dom';
 import {SectionsContainer, Section} from 'react-fullpage';
 import {Link} from 'react-router-dom';
 
@@ -7,62 +6,16 @@ import Cycle from './Cycle';
 import Meteor from './Meteor';
 import LunarEclipse from './Leclipse';
 import SolarEclipse from './Seclipse';
+import Draw from './Draw';
 import Nav from '.././Nav';
 import './events.css';
 
 class Events extends Component {
-    // componentDidMount(){
-    //     window.addEventListener('scroll', this.handleScroll);
-    //     this.canvas = findDOMNode(this.canvasRef);
-    //     this.ctx = this.canvas.getContext('2d');
-    //     this.updateCanvas();
-    // }
-
-    getCursorPosition(e) {
-        var x;
-        var y;
-        if (e.pageX !== undefined && e.pageY !== undefined) {
-            x = e.pageX;
-            y = e.pageY;
-        } else {
-            x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-            y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-        }
-        console.log("get position activated", x, y)
-        return [x, y];
-    }
-
-    drawLine(e) {
-        var x;
-        var y;
-        var clicks = 0;
-        var lastClick = [0, 0];
-        x = this.getCursorPosition(e)[0];
-        y = this.getCursorPosition(e)[1];
-        console.log("draw line activated", x, y)
-        if (clicks !== 1) {
-            clicks++;
-        } else {
-            console.log("triggered by else clicks", clicks, x, y, lastClick)
-            this.ctx.beginPath();
-            this.ctx.moveTo(lastClick[0], lastClick[1]);
-            this.ctx.lineTo(x, y, 6);
-            this.ctx.strokeStyle = '#fff';
-            this.ctx.stroke();
-            clicks = 0;
-        }
-        lastClick = [x, y];
-    };
-
-    updateCanvas() {
-        this.canvas.addEventListener('click', this.drawLine, false);
-        this.ctx.fillRect(0,0, 500, 500);
-    }
 
     render() {
         let options = {
             sectionClassName:     'section',
-            anchors:              ['cycle', 'lunareclipse', 'solareclipse', 'meteor', 'planetary', 'test'],
+            anchors:              ['cycle', 'lunareclipse', 'solareclipse', 'meteor', 'draw'],
             scrollBar:            false,
             navigation:           true,
             verticalAlign:        false,
@@ -98,13 +51,8 @@ class Events extends Component {
                     <Section className="meteor">
                         <Meteor />
                     </Section>
-                    <Section className="planetary">
-                        <h2>Planetary Events</h2>
-                        <p>SVG animation of planets?</p>
-                        <p>uh....the events and dates?</p>
-                    </Section>
-                    <Section className="test-container">
-                        <canvas id="canvas" width="500" height="500" ref={(canvas) => { this.canvasRef = canvas; }}></canvas>
+                    <Section className="draw">
+                        <Draw />
                     </Section>
                 </SectionsContainer>
             </div>
